cde74fc54369c167cc34410e71317e876aeffc16
meet161098 <84460351+meet161098@users.noreply.github.com>
2026-03-09 23:37:58 +0530

    Create monitor1.tf
    Add monitor1

---
+++ monitors/terraform/monitor1.tf
@@ -0,0 +1,11 @@
+resource "datadog_monitor" "monitor1" {
+  name     = "Test Monitor 1"
+  type     = "query alert"
+  query    = "avg(last_5m):sum:test.metric{env:prod} >= 1"
+  message  = "Test monitor message"
+
+  monitor_thresholds {
+    critical = 1
+    warning = 0.5
+  }
+}
