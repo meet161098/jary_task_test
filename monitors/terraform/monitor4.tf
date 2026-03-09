39844ab8add4d98e60003a1bb8d0ceea88853f02
meet161098 <84460351+meet161098@users.noreply.github.com>
2026-03-09 23:49:25 +0530

    Create monitor4.tf
    Add monitor4

---
+++ monitors/terraform/monitor4.tf
@@ -0,0 +1,11 @@
+resource "datadog_monitor" "monitor4" {
+  name     = "Test Monitor 4"
+  type     = "query alert"
+  query    = "avg(last_5m):sum:test.metric{env:prod} >= 1"
+  message  = "Test monitor message"
+
+  monitor_thresholds {
+    critical = 5
+    warning = 2
+  }
+}
